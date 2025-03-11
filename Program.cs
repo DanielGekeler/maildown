5c8844f38c1c284a9d65010a3625c06c68f92a57
Daniel.Gekeler <181665651+DanielGekeler@users.noreply.github.com>
2025-03-11 21:13:39 +0100

    Added CLI with System.Commandline

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.CommandLine;
+using System.Text.RegularExpressions;
 using MailKit;
 using MailKit.Net.Imap;
 using MailKit.Search;
@@ -8,14 +9,48 @@ namespace maildown;
 
 class Program
 {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
-        var target = "mails";
+        var server = new Option<string>(name: "-s", description: "Domain of the IMAP server")
+        { IsRequired = true, ArgumentHelpName = "imap.example.com" };
+
+        var port = new Option<int>(
+            name: "--port",
+            description: "Port of the IMAP server",
+            getDefaultValue: () => 993)
+        { ArgumentHelpName = "port" };
+
+        var user = new Option<string>(name: "-u", description: "IMAP username (usually the email address)")
+        { IsRequired = true, ArgumentHelpName = "user@example.com" };
+
+        var password = new Option<string>(name: "-p", description: "IMAP password")
+        { IsRequired = true, ArgumentHelpName = "password" };
+
+        var target = new Option<string>(
+            name: "-t",
+            description: "Path where emails should be saved to",
+            getDefaultValue: () => ".")
+        { ArgumentHelpName = "path" };
+
+        var maxlength = new Option<int>(
+            name: "--length",
+            description: "Maximum file name length",
+            getDefaultValue: () => 0)
+        { ArgumentHelpName = "n" };
+
+        var rootCommand = new RootCommand("Download all emails your emails from any IMAP server")
+            { server, port, user, password, target, maxlength };
+        rootCommand.SetHandler(DownloadAsync, server, port, user, password, target, maxlength);
+        rootCommand.Invoke(args);
+    }
+
+    private static async Task DownloadAsync(string server, int port, string user, string pass, string target, int maxlength)
+    {
         Directory.CreateDirectory(target);
 
         using var client = new ImapClient();
-        await client.ConnectAsync(args[0], 993, SecureSocketOptions.SslOnConnect);
-        await client.AuthenticateAsync(args[1], args[2]);
+        await client.ConnectAsync(server, port, SecureSocketOptions.SslOnConnect);
+        await client.AuthenticateAsync(user, pass);
 
         var folders = await client.GetFoldersAsync(new FolderNamespace('/', ""));
         foreach (var folder in folders)
@@ -30,7 +65,7 @@ class Program
                 var name = $"{msg.From[0].Name}-{msg.Subject}";
                 name = Regex.Replace(name, @"\s+", "_");
                 name = new string([.. name.Select(c => invalidChars.Contains(c) ? '_' : c)]);
-                if (name.Length > 50) name = name.Substring(0, 50);
+                if (maxlength > 0 && name.Length > maxlength) name = name.Substring(0, maxlength);
 
                 await msg.WriteToAsync(Path.Combine(target, name + ".eml"));
             }
