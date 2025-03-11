c53f43566fd39f0bdc20d80cf6e4af8e74d4151a
Daniel.Gekeler <181665651+DanielGekeler@users.noreply.github.com>
2025-03-11 18:13:04 +0100

    Added mailkit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
-﻿namespace maildown;
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit.Security;
+
+namespace maildown;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using var client = new ImapClient();
+        client.Connect(args[0], 993, SecureSocketOptions.SslOnConnect);
+        client.Authenticate(args[1], args[2]);
+
+        client.Inbox.Open(FolderAccess.ReadOnly);
+        var uids = client.Inbox.Search(SearchQuery.All);
+
+        foreach (var uid in uids)
+        {
+            var message = client.Inbox.GetMessage(uid);
+            message.WriteTo(string.Format("{0}.eml", uid));
+        }
+
+        client.Disconnect(true);
     }
 }
