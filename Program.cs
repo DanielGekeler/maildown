c1b6ca4e3e43efa635b218cfe38b33e817fbca70
Daniel.Gekeler <181665651+DanielGekeler@users.noreply.github.com>
2025-03-12 20:41:48 +0100

    Added --nospaces to choose if spaces in filenames should be replaced

--- Program.cs
+++ Program.cs
@@ -26,9 +26,9 @@ class Program
         var password = new Option<string>(name: "-p", description: "IMAP password")
         { IsRequired = true, ArgumentHelpName = "password" };
 
-        var target = new Option<string>(
-            name: "-t",
-            description: "Path where emails should be saved to",
+        var output = new Option<string>(
+            name: "-o",
+            description: "Output path where emails should be saved to",
             getDefaultValue: () => ".")
         { ArgumentHelpName = "path" };
 
@@ -38,16 +38,18 @@ class Program
             getDefaultValue: () => 0)
         { ArgumentHelpName = "n" };
 
+        var nospaces = new Option<bool>(
+            name: "--nospaces",
+            description: "Replace spaces in filenames with underscores");
+
         var rootCommand = new RootCommand("Download all emails your emails from any IMAP server")
-            { server, port, user, password, target, maxlength };
-        rootCommand.SetHandler(DownloadAsync, server, port, user, password, target, maxlength);
+            { server, port, user, password, output, maxlength, nospaces };
+        rootCommand.SetHandler(DownloadAsync, server, port, user, password, output, maxlength, nospaces);
         rootCommand.Invoke(args);
     }
 
-    private static async Task DownloadAsync(string server, int port, string user, string pass, string target, int maxlength)
+    private static async Task DownloadAsync(string server, int port, string user, string pass, string output, int maxlength, bool nospaces)
     {
-        Directory.CreateDirectory(target);
-
         using var client = new ImapClient();
         await client.ConnectAsync(server, port, SecureSocketOptions.SslOnConnect);
         await client.AuthenticateAsync(user, pass);
@@ -57,17 +59,20 @@ class Program
         {
             await folder.OpenAsync(FolderAccess.ReadOnly);
             await folder.SearchAsync(SearchQuery.All);
-            Console.WriteLine($"Found {folder.Count} messages in {folder.FullName}");
+            var folderName = folder.FullName.Replace(folder.DirectorySeparator, Path.DirectorySeparatorChar);
+            Console.WriteLine($"Found {folder.Count} messages in {folderName}");
+            if (folder.Count == 0) continue;
+            Directory.CreateDirectory(Path.Combine(output, folderName));
 
             var invalidChars = Path.GetInvalidFileNameChars();
             foreach (var msg in folder)
             {
                 var name = $"{msg.From[0].Name}-{msg.Subject}";
-                name = Regex.Replace(name, @"\s+", "_");
+                if (nospaces) name = Regex.Replace(name, @"\s+", "_");
                 name = new string([.. name.Select(c => invalidChars.Contains(c) ? '_' : c)]);
                 if (maxlength > 0 && name.Length > maxlength) name = name.Substring(0, maxlength);
 
-                await msg.WriteToAsync(Path.Combine(target, name + ".eml"));
+                await msg.WriteToAsync(Path.Combine(output, folderName, name + ".eml"));
             }
         }
         await client.DisconnectAsync(true);
